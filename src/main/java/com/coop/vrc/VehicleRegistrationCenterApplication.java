ff906b4ff7844789d808f4d04b0749897c3edefd
Hanan Alhajri <hananmalhajrii@gmail.com>
2025-08-06 13:05:33 +0300

    init commit

---
+++ src/main/java/com/coop/vrc/VehicleRegistrationCenterApplication.java
@@ -0,0 +1,13 @@
+package com.coop.vrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRegistrationCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRegistrationCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coop/vrc/VehicleRegistrationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coop.vrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRegistrationCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
